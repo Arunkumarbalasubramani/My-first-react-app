81fe02890626e629f81d244265440f9be83ffd12
Arunkumarbalasubramani <arunkumar.vbs.b@gmail.com>
2022-09-05 19:22:57 +0530

    Code Updated

--- src/App.js
+++ src/App.js
@@ -1,80 +1,73 @@
-
 import "./App.css";
 
-import { Routes, Route, Link } from "react-router-dom";
-import {  AddColor,ColorBox } from "./color-game.js";
-import { Counter,Movie, Moviepage  } from "./MoviesList";
+import { Routes, Route, Link, Navigate } from "react-router-dom";
+import { AddColor, ColorBox } from "./color-game.js";
+import { Counter, Movie, Moviepage, AddMovies } from "./MoviesList";
 import { Home } from "./Home";
-
+import { useState } from "react";
+import { useParams } from "react-router-dom";
 function App() {
-  const movieList = [
+  const initialMovieList = [
     {
-
       name: "Vikram",
       poster:
-      "https://m.media-amazon.com/images/M/MV5BMmJhYTYxMGEtNjQ5NS00MWZiLWEwN2ItYjJmMWE2YTU1YWYxXkEyXkFqcGdeQXVyMTEzNzg0Mjkx._V1_.jpg",
+        "https://m.media-amazon.com/images/M/MV5BMmJhYTYxMGEtNjQ5NS00MWZiLWEwN2ItYjJmMWE2YTU1YWYxXkEyXkFqcGdeQXVyMTEzNzg0Mjkx._V1_.jpg",
       rating: 8.4,
       summary:
-      "Members of a black ops team must track and eliminate a gang of masked murderers.",
-      
-      },
-      {
-        name: "RRR",
-        poster:
+        "Members of a black ops team must track and eliminate a gang of masked murderers.",
+    },
+    {
+      name: "RRR",
+      poster:
         "https://englishtribuneimages.blob.core.windows.net/gallary-content/2021/6/Desk/2021_6$largeimg_977224513.JPG",
-        rating: 8.8,
-        summary:
+      rating: 8.8,
+      summary:
         "RRR is an upcoming Indian Telugu-language period action drama film directed by S. S. Rajamouli, and produced by D. V. V. Danayya of DVV Entertainments.",
-        
-        },
-        {
-          name: "Iron man 2",
-          poster:
-          "https://m.media-amazon.com/images/M/MV5BMTM0MDgwNjMyMl5BMl5BanBnXkFtZTcwNTg3NzAzMw@@._V1_FMjpg_UX1000_.jpg",
-          rating: 7,
-          summary:
-          "With the world now aware that he is Iron Man, billionaire inventor Tony Stark (Robert Downey Jr.) faces pressure from all sides to share his technology with the military. He is reluctant to divulge the secrets of his armored suit, fearing the information will fall into the wrong hands. With Pepper Potts (Gwyneth Paltrow) and Rhodes (Don Cheadle) by his side, Tony must forge new alliances and confront a powerful new enemy.",
-          
-          },
-          {
-            name: "The Avengers",
-            rating: 8,
-            summary:
-            "Marvel's The Avengers (classified under the name Marvel Avengers\n Assemble in the United Kingdom and Ireland), or simply The Avengers, is\n a 2012 American superhero film based on the Marvel Comics superhero team\n of the same name.",
-            poster:
-            "https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersendgame_lob_crd_05.jpg",
-            
-            },
-          {
-            name: "Jai Bhim",
-            poster:
-            "https://m.media-amazon.com/images/M/MV5BY2Y5ZWMwZDgtZDQxYy00Mjk0LThhY2YtMmU1MTRmMjVhMjRiXkEyXkFqcGdeQXVyMTI1NDEyNTM5._V1_FMjpg_UX1000_.jpg",
-            summary:
-            "A tribal woman and a righteous lawyer battle in court to unravel the mystery around the disappearance of her husband, who was picked up the police on a false case",
-            rating: 8.8,
-            
-            },
-            {
-              name: "Ratatouille",
-              poster:
-              "https://resizing.flixster.com/gL_JpWcD7sNHNYSwI1ff069Yyug=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzLzc4ZmJhZjZiLTEzNWMtNDIwOC1hYzU1LTgwZjE3ZjQzNTdiNy5qcGc=",
-              rating: 8,
-              summary:
-              "Remy, a rat, aspires to become a renowned French chef. However, he fails to realise that people despise rodents and will never enjoy a meal cooked by him.",
-              
-              },
+    },
+    {
+      name: "Iron man 2",
+      poster:
+        "https://m.media-amazon.com/images/M/MV5BMTM0MDgwNjMyMl5BMl5BanBnXkFtZTcwNTg3NzAzMw@@._V1_FMjpg_UX1000_.jpg",
+      rating: 7,
+      summary:
+        "With the world now aware that he is Iron Man, billionaire inventor Tony Stark (Robert Downey Jr.) faces pressure from all sides to share his technology with the military. He is reluctant to divulge the secrets of his armored suit, fearing the information will fall into the wrong hands. With Pepper Potts (Gwyneth Paltrow) and Rhodes (Don Cheadle) by his side, Tony must forge new alliances and confront a powerful new enemy.",
+    },
+    {
+      name: "The Avengers",
+      rating: 8,
+      summary:
+        "Marvel's The Avengers (classified under the name Marvel Avengers\n Assemble in the United Kingdom and Ireland), or simply The Avengers, is\n a 2012 American superhero film based on the Marvel Comics superhero team\n of the same name.",
+      poster:
+        "https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersendgame_lob_crd_05.jpg",
+    },
+    {
+      name: "Jai Bhim",
+      poster:
+        "https://m.media-amazon.com/images/M/MV5BY2Y5ZWMwZDgtZDQxYy00Mjk0LThhY2YtMmU1MTRmMjVhMjRiXkEyXkFqcGdeQXVyMTI1NDEyNTM5._V1_FMjpg_UX1000_.jpg",
+      summary:
+        "A tribal woman and a righteous lawyer battle in court to unravel the mystery around the disappearance of her husband, who was picked up the police on a false case",
+      rating: 8.8,
+    },
+    {
+      name: "Ratatouille",
+      poster:
+        "https://resizing.flixster.com/gL_JpWcD7sNHNYSwI1ff069Yyug=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzLzc4ZmJhZjZiLTEzNWMtNDIwOC1hYzU1LTgwZjE3ZjQzNTdiNy5qcGc=",
+      rating: 8,
+      summary:
+        "Remy, a rat, aspires to become a renowned French chef. However, he fails to realise that people despise rodents and will never enjoy a meal cooked by him.",
+    },
 
-   
     {
       name: "Interstellar",
-      rating: "8.6 ⭐",
-      poster: "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
+      rating: "8.6",
+      poster:
+        "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
       summary:
         "When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans.",
     },
     {
       name: "Justice league",
-      rating: "6.1⭐",
+      rating: "6.1",
       poster:
         "https://images-na.ssl-images-amazon.com/images/I/91JNWWQKGgL._RI_.jpg",
       summary:
@@ -82,14 +75,14 @@ function App() {
     },
     {
       name: "Inception",
-      rating: "8.8 ⭐",
+      rating: "8.8",
       poster: "https://flxt.tmsimg.com/assets/p7825626_p_v10_af.jpg",
       summary:
         "Cobb steals information from his targets by entering their dreams. Saito offers to wipe clean Cobbs criminal history as payment for performing an inception on his sick competitors son.",
     },
     {
       name: "Arjun Reddy",
-      rating: "8.1 ⭐",
+      rating: "8.1 ",
       poster:
         "https://images-na.ssl-images-amazon.com/images/I/91X1PmwPjkL._RI_.jpg",
       summary:
@@ -97,7 +90,7 @@ function App() {
     },
     {
       name: "Master",
-      rating: "7.8 ⭐",
+      rating: "7.8 ",
       poster:
         "https://i2.wp.com/www.socialnews.xyz/wp-content/uploads/2020/03/14/Thalapathy-vijay-s-Master-movie-3rd-single-Vaathi-Raid-will-be-out-today-at-8-30PM-.jpg?quality=90&zoom=1&ssl=1",
       summary:
@@ -105,33 +98,71 @@ function App() {
     },
     {
       name: "Justice league: Snyders Cut",
-      rating: "7.8 ⭐",
+      rating: "7.8 ",
       poster:
         "https://m.media-amazon.com/images/M/MV5BYjI3NDg0ZTEtMDEwYS00YWMyLThjYjktMTNlM2NmYjc1OGRiXkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_.jpg",
       summary:
         "Bruce Wayne and  Diana Prince plans to recruit a team of metahumans to protect the world from an approaching threat of Darkseid. The task proves more difficult than Bruce imagined, as each of the recruits must face the demons of their own pasts. United ; Batman, Wonder Woman, Aquaman, Cyborg and the Flash must save the planet from Steppenwolf, DeSaad and Darkseid and their dreadful intentions",
     },
   ];
+  const [movieList, setMovieList] = useState(initialMovieList);
   return (
     <div className="App">
-
       <ul>
-        <li><Link to="/">Home</Link> </li>
-        <li><Link to="/movies">Movies App</Link> </li>
-        <li><Link to="/color-game">Color Game</Link></li>
- 
+        <li>
+          <Link to="/">Home</Link>{" "}
+        </li>
+        <li>
+          <Link to="/films">Movies App</Link>{" "}
+        </li>
+        <li>
+          <Link to="/color-game">Color Game</Link>
+        </li>
+        <li>
+          <Link to="/add-movies">Not a page </Link>
+        </li>
       </ul>
-        <Routes>
+      <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<Moviepage movieList ={movieList} />} />
-        <Route path="/color-game" element={<AddColor />} />
-      </Routes>
-   
+        <Route path="/films" element={<Navigate replace to="/movies" />} />
+        <Route
+          path="/movies"
+          element={
+            <Moviepage movieList={movieList} setMovieList={setMovieList} />
+          }
+        />
 
-   
+        <Route path="/color-game" element={<AddColor />} />
+        <Route
+          path="/movies/:id"
+          element={<MovieDetails movieList={movieList} />}
+        />
+        <Route path="*" element={<Navigate replace to="/404-not-found" />} />
+        <Route path="/404-not-found" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
 
+function NotFound() {
+  return (
+    <div>
+      <img
+        src="https://cdn.dribbble.com/users/62549/screenshots/3894903/media/c3a11496923b170c18c947fe93d13261.gif"
+        alt="page-not-found"
+        className="not-found"
+      />
+    </div>
+  );
+}
 
+function MovieDetails({ movieList }) {
+  const { id } = useParams();
+  // console.log(movieList);
+  return (
+    <div>
+      <h1>Movie Details {id} .... 🎉🎉🎉</h1>
+    </div>
+  );
+}
 export default App;
--- src/MoviesList.js
+++ src/MoviesList.js
@@ -1,55 +1,119 @@
 import { useState } from "react";
-function Moviepage({movieList}){
+import { useNavigate } from "react-router-dom";
 
-
-    return(
+function Moviepage({ movieList, setMovieList }) {
+  const [name, setName] = useState("");
+  const [rating, setRating] = useState("");
+  const [poster, setPoster] = useState("");
+  const [summary, setSummary] = useState("");
+  return (
+    <div className="movies">
+      <div className="add-movie-form">
+        <h1> You can Add Movies Here 🎉 🎊 🎊 </h1>
+        <div className="input-fields">
+          <input
+            type="text"
+            name="Movie-name"
+            placeholder="Name of the Movie"
+            onChange={(event) => setName(event.target.value)}
+          />
+          <input
+            type="text"
+            name="Movie-rating"
+            placeholder="Rating"
+            onChange={(event) => setRating(event.target.value)}
+          />
+          <input
+            type="text"
+            name="poster"
+            placeholder="Poster"
+            onChange={(event) => setPoster(event.target.value)}
+          />
+          <input
+            type="text"
+            name="Movie-summary"
+            placeholder="summary"
+            onChange={(event) => setSummary(event.target.value)}
+          />
+          <button
+            onClick={() => {
+              const newMovie = {
+                name: name,
+                poster: poster,
+                rating: rating,
+                summary: summary,
+              };
+              setMovieList([...movieList, newMovie]);
+            }}
+          >
+            Add Movie
+          </button>
+        </div>
+      </div>
       <div className="movie-app">
-     
         <section className="movies-list">
-        {movieList.map((element) => (
+          {movieList.map((element, index) => (
             <Movie
               name={element.name}
               rating={element.rating}
               poster={element.poster}
               summary={element.summary}
+              key={index}
+              id={index}
             />
           ))}
-     
         </section>
       </div>
-    )
-  }
+    </div>
+  );
+}
 
-  function Movie(props) {
-    return (
-      <div className="movie-container">
-        <img className="movie-poster" src={props.poster} alt="" />
-        <div className="movie-specs">
-          <h3 className="movie-name">{props.name} </h3>
-          <p className="movie-rating">{props.rating}</p>
-        </div>
-        <div className="movie-summary">
-          <p className="movie-summary">{props.summary}</p>
-        </div>
-        <Counter />
+function Movie(props, id) {
+  const styles = {
+    color: props.rating > 8 ? "green" : "red",
+  };
+  const [show, setShow] = useState(true);
+  const navigate = useNavigate();
+  return (
+    <div className="movie-container">
+      <img className="movie-poster" src={props.poster} alt="" />
+      <div className="movie-specs">
+        <h3 className="movie-name">
+          {props.name}{" "}
+          <button
+            onClick={() => {
+              setShow(!show);
+            }}
+          >
+            Toggle Summary
+          </button>
+          <button onClick={() => navigate(`/movies/${id}`)}>Info</button>
+        </h3>
+        <p style={styles} className="movie-rating">
+          {props.rating}✪
+        </p>
       </div>
-    );
-  }
-  function Counter() {
-    const [like, setLike] = useState(0);
-    const [disLike, setDisLike] = useState(0);
-    return (
-      <div className="counter-container">
-        <button className="like" onClick={() => setLike(like + 1)}>
-        
-          👍 <span>{like}</span>
-        </button>
-        <button className="dislike" onClick={() => setDisLike(disLike + 1)}>
-        
-          👎 <span>{disLike}</span>
-        </button>
+      <div className="movie-summary">
+        {show ? <p className="movie-summary">{props.summary} </p> : null}
       </div>
-    );
-  }
+      <Counter />
+    </div>
+  );
+}
 
-  export{Counter,Movie, Moviepage }
+function Counter() {
+  const [like, setLike] = useState(0);
+  const [disLike, setDisLike] = useState(0);
+  return (
+    <div className="counter-container">
+      <button className="like" onClick={() => setLike(like + 1)}>
+        👍 <span>{like}</span>
+      </button>
+      <button className="dislike" onClick={() => setDisLike(disLike + 1)}>
+        👎 <span>{disLike}</span>
+      </button>
+    </div>
+  );
+}
+
+export { Counter, Movie, Moviepage };
